46e117f0a000f4e3651b16df6ca373b08aa3b229
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2025-09-28 07:50:35 +0000

    express

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+    return res.send("Welcome to auth service!")
+})
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
-const main = (name: string) => {
-    console.log(`welcome ${name}`)
+import { Config } from './config/index.js'
+import app from './app.js'
+
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => console.log(`Server up and running at ${PORT}`));
+    } catch (error) {
+        console.warn(error);
+        process.exit(1);
+    }
 }
 
-main('Vinz Husky')
+startServer();
