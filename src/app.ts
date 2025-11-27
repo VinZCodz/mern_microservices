0a94ffdff5676e09a859696bc8285836fcb98365
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2025-11-27 13:48:27 +0000

    Global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
-import express from 'express'
+import express, {
+    type NextFunction,
+    type Request,
+    type Response,
+} from 'express'
+import { HttpError } from 'http-errors'
+import logger from './config/logger.ts'
 const app = express()
 
 app.get('/', (req, res) => {
     return res.send('Welcome to auth service!')
 })
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.stack)
+
+    res.status(err.statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    })
+})
 export default app
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,6 @@
 import dotenv from 'dotenv'
-import logger from './logger'
 
-const result = dotenv.config()
-
-if (result.error) {
-    logger.error('Error loading .env file:', result.error)
-}
+dotenv.config()
 
 const { PORT, NODE_ENV } = process.env
 
